1d939297859be18ee183515230e6661a24156255
Dawei Zhan <zhandawei@users.noreply.github.com>
2024-11-27 08:48:38 +0800

    udpate

--- Calculate_Pareto_Front.m
+++ Calculate_Pareto_Front.m
@@ -1,7 +1,288 @@
-function pareto_front = Calculate_Pareto_Front(num_n, num_obj)
-f = UniformPoint(num_n,num_obj);
-pareto_front = f./repmat(sqrt(sum(f.^2,2)),1,num_obj);
+function pareto_front = Calculate_Pareto_Front(obj_fun, num_n, num_obj)
+
+switch obj_fun
+    case 'ZDT1'
+        f(:,1)    = (0:1/(num_n-1):1)';
+        f(:,2)    = 1 - f(:,1).^0.5;
+        pareto_front = f;
+    case 'ZDT2'
+        f(:,1)    = (0:1/(num_n-1):1)';
+        f(:,2)    = 1-f(:,1).^2;
+        pareto_front = f;
+    case 'ZDT3'
+        f(:,1)    = (0:1/(num_n-1):1)';
+        f(:,2)    = 1-f(:,1).^0.5-f(:,1).*sin(10*pi*f(:,1));
+        f = f(Front_Rank(f,1)==1,:);
+        pareto_front = f;
+    case 'ZDT4'
+        f(:,1)    = (0:1/(num_n-1):1)';
+        f(:,2)    = 1-f(:,1).^0.5;
+        pareto_front = f;
+    case 'ZDT6'
+        minf1     = 0.280775;
+        f(:,1)    = (minf1:(1-minf1)/(num_n-1):1)';
+        f(:,2)    = 1-f(:,1).^2;
+        pareto_front = f;
+    case 'IDTLZ1'
+        f = UniformPoint(num_n,num_obj)/2;
+        pareto_front = (1-f)/2;
+    case 'IDTLZ2'
+        f = UniformPoint(num_n,num_obj);
+        pareto_front = 1 - f./repmat(sqrt(sum(f.^2,2)),1,num_obj);
+    case 'DTLZ1'
+        f = UniformPoint(num_n,num_obj)/2;
+        pareto_front = f;
+    case 'DTLZ2'
+        f = UniformPoint(num_n,num_obj);
+        pareto_front = f./repmat(sqrt(sum(f.^2,2)),1,num_obj);
+    case 'DTLZ3'
+        f = UniformPoint(num_n,num_obj);
+        pareto_front = f./repmat(sqrt(sum(f.^2,2)),1,num_obj);
+    case 'DTLZ4'
+        f = UniformPoint(num_n,num_obj);
+        pareto_front = f./repmat(sqrt(sum(f.^2,2)),1,num_obj);
+    case 'DTLZ5'
+        f = [0:1/(num_n-1):1;1:-1/(num_n-1):0]';
+        f = f./repmat(sqrt(sum(f.^2,2)),1,size(f,2));
+        f = [f(:,ones(1,num_obj-2)),f];
+        pareto_front = f./sqrt(2).^repmat([num_obj-2,num_obj-2:-1:0],size(f,1),1);
+    case 'DTLZ6'
+        f = [0:1/(num_n-1):1;1:-1/(num_n-1):0]';
+        f = f./repmat(sqrt(sum(f.^2,2)),1,size(f,2));
+        f = [f(:,ones(1,num_obj-2)),f];
+        pareto_front = f./sqrt(2).^repmat([num_obj-2,num_obj-2:-1:0],size(f,1),1);
+    case 'DTLZ7'
+        interval     = [0,0.251412,0.631627,0.859401];
+        median       = (interval(2)-interval(1))/(interval(4)-interval(3)+interval(2)-interval(1));
+        %         X            = ReplicatePoint(10000,num_obj-1);
+        if num_obj  > 2
+            sample_num = (ceil(num_n^(1/(num_obj - 1))))^(num_obj - 1);
+            Gap       = 0:1/(sample_num^(1/(num_obj - 1))-1):1;
+            eval(sprintf('[%s]=ndgrid(Gap);',sprintf('c%d,',1:(num_obj - 1))))
+            eval(sprintf('X=[%s];',sprintf('c%d(:),',1:(num_obj - 1))))
+        else
+            X = (0:1/(num_n-1):1)';
+        end
+        X(X<=median) = X(X<=median)*(interval(2)-interval(1))/median+interval(1);
+        X(X>median)  = (X(X>median)-median)*(interval(4)-interval(3))/(1-median)+interval(3);
+        pareto_front            = [X,2*(num_obj-sum(X/2.*(1+sin(3*pi.*X)),2))];
+    case 'WFG1'
+        R = UniformPoint(num_n,num_obj);
+        c = ones(size(R,1),num_obj);
+        for i = 1 : size(R,1)
+            for j = 2 : num_obj
+                temp = R(i,j)/R(i,1)*prod(1-c(i,num_obj-j+2:num_obj-1));
+                c(i,num_obj-j+1) = (temp^2-temp+sqrt(2*temp))/(temp^2+1);
+            end
+        end
+        x = acos(c)*2/pi;
+        temp = (1-sin(pi/2*x(:,2))).*R(:,num_obj)./R(:,num_obj-1);
+        a = 0 : 0.0001 : 1;
+        E = abs(temp*(1-cos(pi/2*a))-1+repmat(a+cos(10*pi*a+pi/2)/10/pi,size(x,1),1));
+        [~,rank] = sort(E,2);
+        for i = 1 : size(x,1)
+            x(i,1) = a(min(rank(i,1:10)));
+        end
+        R      = fliplr(cumprod([ones(size(x,1),1),1-cos(x(:,1:end-1)*pi/2)],2)).*[ones(size(x,1),1),1-sin(x(:,end-1:-1:1)*pi/2)];
+        R(:,num_obj) = 1-x(:,1)-cos(10*pi*x(:,1)+pi/2)/10/pi;
+        pareto_front    = repmat(2:2:2*num_obj,size(R,1),1).*R;
+    case 'WFG2'
+        R = UniformPoint(num_n,num_obj);
+        c = ones(size(R,1),num_obj);
+        for i = 1 : size(R,1)
+            for j = 2 : num_obj
+                temp = R(i,j)/R(i,1)*prod(1-c(i,num_obj-j+2:num_obj-1));
+                c(i,num_obj-j+1) = (temp^2-temp+sqrt(2*temp))/(temp^2+1);
+            end
+        end
+        x = acos(c)*2/pi;
+        temp = (1-sin(pi/2*x(:,2))).*R(:,num_obj)./R(:,num_obj-1);
+        a = 0 : 0.0001 : 1;
+        E = abs(temp*(1-cos(pi/2*a))-1+repmat(a.*cos(5*pi*a).^2,size(x,1),1));
+        [~,rank] = sort(E,2);
+        for i = 1 : size(x,1)
+            x(i,1) = a(min(rank(i,1:10)));
+        end
+        R      = convex(x);
+        R(:,num_obj) = disc(x);
+        R      = R(NDSort(R,1)==1,:);
+        pareto_front    = repmat(2:2:2*num_obj,size(R,1),1).*R;
+    case 'WFG3'
+        X = linspace(0,1,num_n)';
+        X = [X,zeros(num_n,num_obj-2)+0.5,zeros(num_n,1)];
+        R = linear(X);
+        pareto_front = repmat(2:2:2*num_obj,size(R,1),1).*R;
+    case 'WFG4'
+        R = UniformPoint(num_n,num_obj);
+        R = R./repmat(sqrt(sum(R.^2,2)),1,num_obj);
+        pareto_front = repmat(2:2:2*num_obj,size(R,1),1).*R;
+    case 'WFG5'
+        R = UniformPoint(num_n,num_obj);
+        R = R./repmat(sqrt(sum(R.^2,2)),1,num_obj);
+        pareto_front = repmat(2:2:2*num_obj,size(R,1),1).*R;
+    case 'WFG6'
+        R = UniformPoint(num_n,num_obj);
+        R = R./repmat(sqrt(sum(R.^2,2)),1,num_obj);
+        pareto_front = repmat(2:2:2*num_obj,size(R,1),1).*R;
+    case 'WFG7'
+        R = UniformPoint(num_n,num_obj);
+        R = R./repmat(sqrt(sum(R.^2,2)),1,num_obj);
+        pareto_front = repmat(2:2:2*num_obj,size(R,1),1).*R;
+    case 'WFG8'
+        R = UniformPoint(num_n,num_obj);
+        R = R./repmat(sqrt(sum(R.^2,2)),1,num_obj);
+        pareto_front = repmat(2:2:2*num_obj,size(R,1),1).*R;
+    case 'WFG9'
+        R = UniformPoint(num_n,num_obj);
+        R = R./repmat(sqrt(sum(R.^2,2)),1,num_obj);
+        pareto_front = repmat(2:2:2*num_obj,size(R,1),1).*R;
+        
 end
+end
+
+
+
+
+
+
+
+
+function Output = linear(x)
+Output = fliplr(cumprod([ones(size(x,1),1),x(:,1:end-1)],2)).*[ones(size(x,1),1),1-x(:,end-1:-1:1)];
+end
+
+function Output = convex(x)
+Output = fliplr(cumprod([ones(size(x,1),1),1-cos(x(:,1:end-1)*pi/2)],2)).*[ones(size(x,1),1),1-sin(x(:,end-1:-1:1)*pi/2)];
+end
+
+function Output = disc(x)
+Output = 1-x(:,1).*(cos(5*pi*x(:,1))).^2;
+end
+
+function [FrontNo,MaxFNo] = NDSort(varargin)
+PopObj = varargin{1};
+[N,M]  = size(PopObj);
+if nargin == 2
+    nSort  = varargin{2};
+else
+    PopCon = varargin{2};
+    nSort  = varargin{3};
+    Infeasible           = any(PopCon>0,2);
+    PopObj(Infeasible,:) = repmat(max(PopObj,[],1),sum(Infeasible),1) + repmat(sum(max(0,PopCon(Infeasible,:)),2),1,M);
+end
+if M < 3 || N < 500
+    % Use efficient non-dominated sort with sequential search (ENS-SS)
+    [FrontNo,MaxFNo] = ENS_SS(PopObj,nSort);
+else
+    % Use tree-based efficient non-dominated sort (T-ENS)
+    [FrontNo,MaxFNo] = T_ENS(PopObj,nSort);
+end
+end
+
+function [FrontNo,MaxFNo] = ENS_SS(PopObj,nSort)
+[PopObj,~,Loc] = unique(PopObj,'rows');
+Table   = hist(Loc,1:max(Loc));
+[N,M]   = size(PopObj);
+FrontNo = inf(1,N);
+MaxFNo  = 0;
+while sum(Table(FrontNo<inf)) < min(nSort,length(Loc))
+    MaxFNo = MaxFNo + 1;
+    for i = 1 : N
+        if FrontNo(i) == inf
+            Dominated = false;
+            for j = i-1 : -1 : 1
+                if FrontNo(j) == MaxFNo
+                    m = 2;
+                    while m <= M && PopObj(i,m) >= PopObj(j,m)
+                        m = m + 1;
+                    end
+                    Dominated = m > M;
+                    if Dominated || M == 2
+                        break;
+                    end
+                end
+            end
+            if ~Dominated
+                FrontNo(i) = MaxFNo;
+            end
+        end
+    end
+end
+FrontNo = FrontNo(:,Loc);
+end
+
+function [FrontNo,MaxFNo] = T_ENS(PopObj,nSort)
+[PopObj,~,Loc] = unique(PopObj,'rows');
+Table     = hist(Loc,1:max(Loc));
+[N,M]     = size(PopObj);
+FrontNo   = inf(1,N);
+MaxFNo    = 0;
+Forest    = zeros(1,N);
+Children  = zeros(N,M-1);
+LeftChild = zeros(1,N) + M;
+Father    = zeros(1,N);
+Brother   = zeros(1,N) + M;
+[~,ORank] = sort(PopObj(:,2:M),2,'descend');
+ORank     = ORank + 1;
+while sum(Table(FrontNo<inf)) < min(nSort,length(Loc))
+    MaxFNo = MaxFNo + 1;
+    root   = find(FrontNo==inf,1);
+    Forest(MaxFNo) = root;
+    FrontNo(root)  = MaxFNo;
+    for p = 1 : N
+        if FrontNo(p) == inf
+            Pruning = zeros(1,N);
+            q = Forest(MaxFNo);
+            while true
+                m = 1;
+                while m < M && PopObj(p,ORank(q,m)) >= PopObj(q,ORank(q,m))
+                    m = m + 1;
+                end
+                if m == M
+                    break;
+                else
+                    Pruning(q) = m;
+                    if LeftChild(q) <= Pruning(q)
+                        q = Children(q,LeftChild(q));
+                    else
+                        while Father(q) && Brother(q) > Pruning(Father(q))
+                            q = Father(q);
+                        end
+                        if Father(q)
+                            q = Children(Father(q),Brother(q));
+                        else
+                            break;
+                        end
+                    end
+                end
+            end
+            if m < M
+                FrontNo(p) = MaxFNo;
+                q = Forest(MaxFNo);
+                while Children(q,Pruning(q))
+                    q = Children(q,Pruning(q));
+                end
+                Children(q,Pruning(q)) = p;
+                Father(p) = q;
+                if LeftChild(q) > Pruning(q)
+                    Brother(p)   = LeftChild(q);
+                    LeftChild(q) = Pruning(q);
+                else
+                    bro = Children(q,LeftChild(q));
+                    while Brother(bro) < Pruning(q)
+                        bro = Children(q,Brother(bro));
+                    end
+                    Brother(p)   = Brother(bro);
+                    Brother(bro) = Pruning(q);
+                end
+            end
+        end
+    end
+end
+FrontNo = FrontNo(:,Loc);
+end
+
+
 
 function [W,N] = UniformPoint(N,M)
 %UniformPoint - Generate a set of uniformly distributed points on the unit
@@ -12,6 +293,7 @@ function [W,N] = UniformPoint(N,M)
 %
 %   Example:
 %       [W,N] = UniformPoint(275,10)
+
 %--------------------------------------------------------------------------
 % Copyright (c) 2016-2017 BIMK Group. You are free to use the PlatEMO for
 % research purposes. All publications which use this platform or any code
@@ -20,6 +302,7 @@ function [W,N] = UniformPoint(N,M)
 % for Evolutionary Multi-Objective Optimization [Educational Forum], IEEE
 % Computational Intelligence Magazine, 2017, 12(4): 73-87".
 %--------------------------------------------------------------------------
+
 H1 = 1;
 while nchoosek(H1+M,M-1) <= N
     H1 = H1 + 1;
@@ -40,4 +323,3 @@ end
 W = max(W,1e-6);
 N = size(W,1);
 end
-
--- pEHVI_EGO.m
+++ pEHVI_EGO.m
@@ -1,50 +1,45 @@
 clearvars;close all;
 % objective function
-fun_name = 'DTLZ2';
+fun_name = 'DTLZ7';
 % number of objectives
-num_obj = 3;
+num_obj = 2;
 % number of variables
-num_vari = 10;
+num_vari = 6;
 % lower and upper bounds
 lower_bound = zeros(1,num_vari);
 upper_bound = ones(1,num_vari);
 % Pareto front of the test problem
-pareto_front = Calculate_Pareto_Front(1000,num_obj);
+pareto_front = Calculate_Pareto_Front('DTLZ7',10000,num_obj);
 % number of initial design points
-num_initial = 100;
+num_initial = 11*num_vari-1;
 % number of maximal evaluations
 max_evaluation = 200;
 % generate initial design points
 sample_x = lower_bound + (upper_bound-lower_bound).*lhsdesign(num_initial, num_vari,'criterion','maximin','iterations',1000);
 sample_y = feval(fun_name, sample_x, num_obj);
 % normalize the objective values
-sample_y_scaled = (sample_y - min(sample_y))./(max(sample_y)-min(sample_y));
 iteration = 0;
 evaluation = size(sample_x,1);
 kriging_obj = cell(1,num_obj);
 % get current non-dominated front
 [non_dominated_front,index] = Paretoset(sample_y);
-% normalize non-dominated front
-non_dominated_front_scaled = sample_y_scaled(index,:);
 fprintf('pEHVI-EGO on m=%d %s function, iteration: %d, evaluation: %d, IGD: %f\n',num_obj,fun_name,iteration,evaluation,mean(min(pdist2(pareto_front,non_dominated_front),[],2)));
 while evaluation < max_evaluation
     for ii = 1:num_obj
-        kriging_obj{ii} = Kriging_Train(sample_x,sample_y_scaled(:,ii),lower_bound,upper_bound,1*ones(1,num_vari),0.001*ones(1,num_vari),1000*ones(1,num_vari));
+        kriging_obj{ii} = Kriging_Train(sample_x,sample_y(:,ii),lower_bound,upper_bound,1*ones(1,num_vari),0.001*ones(1,num_vari),1000*ones(1,num_vari));
     end
     % get a new point by maximizing pEHVI function using GA
-    [infill_x,~] = Optimizer_GA(@(x)-Infill_pEHVI(x,kriging_obj,non_dominated_front_scaled),num_vari,lower_bound,upper_bound,40,50);
+    [infill_x,~] = Optimizer_GA(@(x)-Infill_pEHVI(x,kriging_obj,non_dominated_front),num_vari,lower_bound,upper_bound,40,50);
     % evaluate the new point using the real objective function
     infill_y = feval(fun_name,infill_x,num_obj);
     sample_x = [sample_x;infill_x];
     sample_y = [sample_y;infill_y];
-    sample_y_scaled = (sample_y - min(sample_y))./(max(sample_y)-min(sample_y));
     evaluation = evaluation + size(infill_x,1);
     iteration = iteration + 1;
     [non_dominated_front,index] = Paretoset(sample_y);
-    non_dominated_front_scaled = sample_y_scaled(index,:);
     fprintf('pEHVI-EGO on m=%d %s function, iteration: %d, evaluation: %d, IGD: %f\n',num_obj,fun_name,iteration,evaluation,mean(min(pdist2(pareto_front,non_dominated_front),[],2)));
 end
 
-fprintf('final HV of non-dominated points: %f\n', Hypervolume(non_dominated_front,2*ones(1,num_obj)));
+
 
 
